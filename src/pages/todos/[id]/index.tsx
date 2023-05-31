9fceb9e2656b289d8a1e06b93bb48c82695c088c
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-05-31 10:11:22 +0900

    更新

--- src/pages/todos/[id]/index.tsx
+++ src/pages/todos/[id]/index.tsx
@@ -35,7 +35,6 @@ export async function getServerSideProps(context: any) {
   return { props: { data } };
 }
 
-// コメント
 const Index = ({ data }: any) => {
   const todo = data.getTodo;
   const [status, setStatus] = useState(todo.status);
